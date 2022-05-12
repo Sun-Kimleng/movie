c8ba76af7e2a28ac350b42e7b16a8bd83a1bcb75
Sun-Kimleng <lengleng44448888@gmail.com>
2022-05-12 22:56:52 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,27 +1,153 @@
 import logo from './logo.svg';
 import './App.css';
-import { Route, Routes } from 'react-router-dom';
+import { Link, NavLink, Route, Routes } from 'react-router-dom';
 import Home from './components/home/home';
 import Header from './components/header/header';
 import Footer from './components/footer/footer';
 import MovieDetail from './components/movie-detail/movieDetail';
 import NotFound from './components/404/notFound';
+import { motion, AnimatePresence } from 'framer-motion';
+import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faBars, faXmark} from '@fortawesome/free-solid-svg-icons';
 
 function App() {
   
+  const [isOpen, setIsOpen] = useState(false);
+
+  const menu =[
+    {
+      path:'/category1',
+      title: 'category One',
+      icon: faBars,
+    },
+    {
+      path:'/category2',
+      title: 'category Two',
+      icon: faBars,
+    },
+    {
+      path:'/category3',
+      title: 'category Three',
+      icon: faBars,
+    }
+
+  ];
+
+  const handleSideBar=()=>{
+    setIsOpen(!isOpen);
+  }
+
+  const bodyVariant ={
+    leftBarOpen: {
+      marginLeft: isOpen ? 310 : 30,
+      transition :{
+        duration: 0.5,
+        type: 'tween',
+        
+      }  
+    },
+
+    leftBarOff: {
+      marginLeft: 30,
+      transition :{
+        duration: 0.5,
+        type: 'tween',
+      }
+  }
+}
+
+  const leftBarVariant = {
+    show: {
+      width: isOpen ? 300 : 30,
+      transition :{
+        duration: 0.5,
+        type: 'tween',
+        
+      }  
+    },
+
+    hide: {
+      width: 30,
+      transition :{
+        duration: 0.5,
+        type: 'tween',
+      }
+    },
+
+    exit:{
+      scale: 0,
+      transition :{
+        duration: 0.5,
+        type: 'spring',
+      }
+    },
+
+    visible: {
+      scale: 1,
+      transition :{
+        duration: 1,
+        type: 'tween',
+      }
+    }
+  }
+
   return (
     <div className="App">
-    <Header />
+      <Header />
+      <div className="content">
+    
+      <motion.div className="left-bar"
+      variants={leftBarVariant}
+      initial='hide'
+      animate='show'
+      >
+        <div className='Icon'>
+       <FontAwesomeIcon className="listIcon" onClick={handleSideBar} icon={isOpen?faXmark:faBars} />
+        </div>
+      <br />
+        <div className="all-category">
+          {
+            menu.map((menu) =>(
+              <NavLink className="list"  to={menu.path}>
+                <FontAwesomeIcon className="listIcon" icon={menu.icon}/>
+                <AnimatePresence>
+                {isOpen && <motion.div className="title"
+                  variants={leftBarVariant}
+                  animate="visible"
+                  initial="exit"
+                  exit="exit"
+                >
+                {menu.title}
+                </motion.div>}
+                </AnimatePresence>
+                <br />
+
+              </NavLink>
+              
+              )
+            )
+          }
+        </div>
+      </motion.div>
+      
+
+      <motion.div className="body" onClick={()=>setIsOpen(false)}
+          variants={bodyVariant}
+          initial='leftBarOff'
+          animate='leftBarOpen'
+      >
         <Routes>
             <Route exact path="/" element={<Home />} />
             <Route path="/movie/:imdbID" element={<MovieDetail />} />
-
             <Route path="*" element={<NotFound />} />
         </Routes>
-   
-    <Footer />
-        
-    
+      
+       
+        </motion.div>
+      </div>
+
+      <Footer />
     </div>
   );
 }
