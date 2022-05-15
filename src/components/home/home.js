bac588903ae4242f1b401b326c9350cf88c41d75
Sun-Kimleng <lengleng44448888@gmail.com>
2022-05-15 19:24:31 +0700

    update

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -3,7 +3,7 @@ import {ApiKey} from '../../common/apis/movieApiKey';
 import movieApi from "../../common/apis/movieApi";
 import { useDispatch } from "react-redux";
 import MovieListing from "../movie-listing/movieListing";
-import { addMovie } from "../../features/movies/movieSlice";
+import { addMovie, fetchAsyncMovies } from "../../features/movies/movieSlice";
 import './home.css'
 
 const Home = () => {
@@ -12,27 +12,16 @@ const Home = () => {
     const dispatch = useDispatch();
 
     const [isPending, setIsPending] = useState(true);
-    const searchText = 'the rock';
-    useEffect(()=>{
-        const fetchMovie = async()=>{
-            await movieApi.get(`?apikey=${ApiKey}&s=${searchText}&type=movie`)
-            .then(response=>{
-                dispatch(addMovie(response.data.Search));
-                console.log(response.data.Search);
-                setIsPending(false);
-            })
-            .catch(error=>{
-                console.log(error);
-            });
-        };
-                fetchMovie()
+    
+    useEffect(()=>{ 
+        dispatch(fetchAsyncMovies());
         
     },[]);
 
     return ( 
         
         <div className="home">
-            {isPending &&<div className="spinner-parent"><div className="parent"> <div className="spinner"></div></div></div>}
+            {/* {isPending &&<div className="spinner-parent"><div className="parent"> <div className="spinner"></div></div></div>} */}
 
             <MovieListing />
         </div>
--- src/features/movies/movieSlice.js
+++ src/features/movies/movieSlice.js
@@ -1,4 +1,19 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import movieApi from "../../common/apis/movieApi";
+import {ApiKey} from '../../common/apis/movieApiKey';
+import { useDispatch } from "react-redux";
+
+
+   
+//create asyncthunk to allow function to access everywher
+//naming ([indentifier-state/function-name])
+
+
+export const fetchAsyncMovies = createAsyncThunk('movies/fetchAsynceMovie', async()=>{
+    const searchText = 'the rock';
+    const response = await movieApi.get(`?apikey=${ApiKey}&s=${searchText}&type=movie`);
+            return response.data.Search;
+})
 
 //first initial state
 const initialState = {
@@ -22,8 +37,26 @@ const movieSlice = createSlice({
         setIsPending: (state)=>{
             state.isPending = false;
         }
-    }
+    },
+    //for creating function not state
+    extraReducers: {
+        //function name
+        [fetchAsyncMovies.pending]: ()=>{
+            console.log('it is pending');
+           
+        },
 
+        [fetchAsyncMovies.fulfilled]: (state, {payload})=>{
+            console.log('succesfully');
+            console.log(payload);
+            //it the same as state.movies=payload
+            return {...state, movies: payload };
+        },
+
+        [fetchAsyncMovies.rejected]: ()=>{
+            console.log('rejected');
+        }
+    }
 }
 );
 
